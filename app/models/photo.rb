62adcadde3d2273567320da9818a6265d34c5c29
Apprentice <apprentice@devbootcamp.com>
2013-03-15 15:54:04 -0700

    Add specs for photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,5 @@ class Photo < ActiveRecord::Base
   belongs_to :inspiration 
   attr_accessible :image, :caption
   mount_uploader :image, ImageUploader
+  validates :caption, :length => {:maximum => 140}
 end
