16dcd30bea3858ec10102c0d3c7ab54d901517f9
Apprentice <apprentice@devbootcamp.com>
2013-03-15 18:14:05 -0700

    added users

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.11'
 gem 'sqlite3'
 gem 'carrierwave_direct'
 gem 'rmagick'
+gem 'devise'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130315222318) do
+ActiveRecord::Schema.define(:version => 20130316005516) do
 
   create_table "inspirations", :force => true do |t|
     t.string   "content"
@@ -26,5 +26,23 @@ ActiveRecord::Schema.define(:version => 20130315222318) do
     t.integer  "inspiration_id"
     t.string   "caption"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
