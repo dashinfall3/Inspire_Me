fc72effea5efd57d2347be83759d7d63c97332d4
Brayden Cleary <brayden.cleary@gmail.com>
2013-03-22 11:43:38 -0700

    Creating signup mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 InspireMe::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => "app13808527@heroku.com",
+  :password => "dxw3tsqt",
+  :domain => "http://www.frozen-cove-1468.herokuapp.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
